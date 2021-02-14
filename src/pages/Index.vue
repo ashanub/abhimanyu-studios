8f53879aa91f1f4e25e96400d31a54f179d3c470
Ashan Boralugoda <ashanudayanga@gmail.com>
2021-02-14 13:46:19 +0530

    removed section scroll

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -23,11 +23,11 @@
     },
     data () {
       return {
-        inMove: false,
-        activeSection: 0,
-        offsets: [],
-        touchStartY: 0,
-        sectionScrollState: true,
+        // inMove: false,
+        // activeSection: 0,
+        // offsets: [],
+        // touchStartY: 0,
+        // sectionScrollState: true,
       }
     },
     components: {
@@ -35,116 +35,116 @@
       AboutMe,
       Contact,
     },
-    methods: {
-      sectionScroll (theEvent) {
-        this.sectionScrollState = !theEvent
-      },
-      calculateSectionOffsets () {
-        if (process.isClient) {
-          let sections = document.getElementsByTagName('section')
-          let length = sections.length
-
-          for (let i = 0; i < length; i++) {
-            let sectionOffset = sections[i].offsetTop
-            this.offsets.push(sectionOffset)
-          }
-        }
-      },
-      handleMouseWheel: function (e) {
-        if (this.sectionScrollState) {
-          if (e.wheelDelta < 30 && !this.inMove) {
-            this.moveUp()
-          } else if (e.wheelDelta > 30 && !this.inMove) {
-            this.moveDown()
-          }
-
-          e.preventDefault()
-        }
-        return false
-      },
-      handleMouseWheelDOM: function (e) {
-
-        if (e.detail > 0 && !this.inMove) {
-          this.moveUp()
-        } else if (e.detail < 0 && !this.inMove) {
-          this.moveDown()
-        }
-
-        return false
-      },
-      moveDown () {
-        this.inMove = true
-        this.activeSection--
-        if (process.isClient) {
-          if (this.activeSection < 0) this.activeSection = (document.querySelectorAll('.fullpage').length * 2) - 1
-        }
-
-        this.scrollToSection(this.activeSection, true)
-      },
-      moveUp () {
-        this.inMove = true
-        this.activeSection++
-        if (process.isClient) {
-          if (this.activeSection > (document.querySelectorAll('.fullpage').length * 2) - 1) this.activeSection = 0
-        }
-
-        this.scrollToSection(this.activeSection, true)
-      },
-      scrollToSection (id, force = false) {
-        if (this.inMove && !force) return false
-
-        this.activeSection = id
-        this.inMove = true
-        if (process.isClient) {
-          document.getElementsByTagName('section')[id].scrollIntoView({ behavior: 'smooth' })
-        }
-
-        setTimeout(() => {
-          this.inMove = false
-        }, 400)
-
-      },
-      touchStart (e) {
-        e.preventDefault()
-
-        this.touchStartY = e.touches[0].clientY
-      },
-      touchMove (e) {
-        if (this.inMove) return false
-        e.preventDefault()
-
-        const currentY = e.touches[0].clientY
-
-        if (this.touchStartY < currentY) {
-          this.moveDown()
-        } else {
-          this.moveUp()
-        }
-
-        this.touchStartY = 0
-        return false
-      },
-    },
-    created () {
-      this.calculateSectionOffsets()
-
-      if (process.isClient) {
-        window.addEventListener('DOMMouseScroll', this.handleMouseWheelDOM)  // Mozilla Firefox
-        window.addEventListener('mousewheel', this.handleMouseWheel, { passive: false }) // Other browsers
-
-        window.addEventListener('touchstart', this.touchStart, { passive: false }) // mobile devices
-        window.addEventListener('touchmove', this.touchMove, { passive: false }) // mobile devices
-      }
-    },
-    destroyed () {
-      if (process.isClient) {
-        window.removeEventListener('mousewheel', this.handleMouseWheel, { passive: false })  // Other browsers
-        window.removeEventListener('DOMMouseScroll', this.handleMouseWheelDOM) // Mozilla Firefox
-
-        window.removeEventListener('touchstart', this.touchStart) // mobile devices
-        window.removeEventListener('touchmove', this.touchMove) // mobile devices
-      }
-    },
+    // methods: {
+    //   sectionScroll (theEvent) {
+    //     this.sectionScrollState = !theEvent
+    //   },
+    //   calculateSectionOffsets () {
+    //     if (process.isClient) {
+    //       let sections = document.getElementsByTagName('section')
+    //       let length = sections.length
+    //
+    //       for (let i = 0; i < length; i++) {
+    //         let sectionOffset = sections[i].offsetTop
+    //         this.offsets.push(sectionOffset)
+    //       }
+    //     }
+    //   },
+    //   handleMouseWheel: function (e) {
+    //     if (this.sectionScrollState) {
+    //       if (e.wheelDelta < 30 && !this.inMove) {
+    //         this.moveUp()
+    //       } else if (e.wheelDelta > 30 && !this.inMove) {
+    //         this.moveDown()
+    //       }
+    //
+    //       e.preventDefault()
+    //     }
+    //     return false
+    //   },
+    //   handleMouseWheelDOM: function (e) {
+    //
+    //     if (e.detail > 0 && !this.inMove) {
+    //       this.moveUp()
+    //     } else if (e.detail < 0 && !this.inMove) {
+    //       this.moveDown()
+    //     }
+    //
+    //     return false
+    //   },
+    //   moveDown () {
+    //     this.inMove = true
+    //     this.activeSection--
+    //     if (process.isClient) {
+    //       if (this.activeSection < 0) this.activeSection = (document.querySelectorAll('.fullpage').length * 2) - 1
+    //     }
+    //
+    //     this.scrollToSection(this.activeSection, true)
+    //   },
+    //   moveUp () {
+    //     this.inMove = true
+    //     this.activeSection++
+    //     if (process.isClient) {
+    //       if (this.activeSection > (document.querySelectorAll('.fullpage').length * 2) - 1) this.activeSection = 0
+    //     }
+    //
+    //     this.scrollToSection(this.activeSection, true)
+    //   },
+    //   scrollToSection (id, force = false) {
+    //     if (this.inMove && !force) return false
+    //
+    //     this.activeSection = id
+    //     this.inMove = true
+    //     if (process.isClient) {
+    //       document.getElementsByTagName('section')[id].scrollIntoView({ behavior: 'smooth' })
+    //     }
+    //
+    //     setTimeout(() => {
+    //       this.inMove = false
+    //     }, 400)
+    //
+    //   },
+    //   touchStart (e) {
+    //     e.preventDefault()
+    //
+    //     this.touchStartY = e.touches[0].clientY
+    //   },
+    //   touchMove (e) {
+    //     if (this.inMove) return false
+    //     e.preventDefault()
+    //
+    //     const currentY = e.touches[0].clientY
+    //
+    //     if (this.touchStartY < currentY) {
+    //       this.moveDown()
+    //     } else {
+    //       this.moveUp()
+    //     }
+    //
+    //     this.touchStartY = 0
+    //     return false
+    //   },
+    // },
+    // created () {
+    //   this.calculateSectionOffsets()
+    //
+    //   if (process.isClient) {
+    //     window.addEventListener('DOMMouseScroll', this.handleMouseWheelDOM)  // Mozilla Firefox
+    //     window.addEventListener('mousewheel', this.handleMouseWheel, { passive: false }) // Other browsers
+    //
+    //     window.addEventListener('touchstart', this.touchStart, { passive: false }) // mobile devices
+    //     window.addEventListener('touchmove', this.touchMove, { passive: false }) // mobile devices
+    //   }
+    // },
+    // destroyed () {
+    //   if (process.isClient) {
+    //     window.removeEventListener('mousewheel', this.handleMouseWheel, { passive: false })  // Other browsers
+    //     window.removeEventListener('DOMMouseScroll', this.handleMouseWheelDOM) // Mozilla Firefox
+    //
+    //     window.removeEventListener('touchstart', this.touchStart) // mobile devices
+    //     window.removeEventListener('touchmove', this.touchMove) // mobile devices
+    //   }
+    // },
   }
 </script>
 
